e337b9e20cd3518972b68e563cefc572a373ce69
Darvel <marsonyteam@gmail.com>
2024-04-06 18:26:01 +0200

    1.3.2 Formatowania wszystkich plików z folderu /src/main/java

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,14 @@
 package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
-
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-    
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer(); 
     @Override
     public int countOut() {
         while (!callCheck())
-            
-        temp.countIn(super.countOut());
-        
-        int ret = temp.countOut();
-        
-        while (!temp.callCheck())
-            
-        countIn(temp.countOut());
-        
+            temp.countIn(super.countOut());
+        int ret = temp.countOut(); 
+        while (!temp.callCheck())    
+            countIn(temp.countOut());
         return ret;
     }
 }
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,6 +5,7 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     public int reportRejected() {
         return totalRejected;
     }
+    
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
