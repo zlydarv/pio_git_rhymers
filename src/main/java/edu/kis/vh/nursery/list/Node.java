1b66ab95d8f8616bef83bba00f8c684aa429a89b
Ajeszny <245107@edu.p.lodz.pl>
2024-06-05 11:52:26 +0200

    11. Hermetyzacja

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,36 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev, next;
+
+    public Node (int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+     public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
     public Node(int i) {
         value = i;
