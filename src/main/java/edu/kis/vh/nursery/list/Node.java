d8655bce9bdfe9c6c8aaa0fe70e654c19ef8bd1b
Ajeszny <245107@edu.p.lodz.pl>
2024-06-05 11:53:27 +0200

    12. Usunięto settery

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -13,24 +13,12 @@ public class Node {
         return value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
-
      public Node getPrev() {
         return prev;
     }
 
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
     public Node getNext() {
         return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
     }
 
     public Node(int i) {
